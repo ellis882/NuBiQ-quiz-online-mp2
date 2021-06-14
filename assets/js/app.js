4ef9a1f5160a37a8f6bf256689397228c8c87819
ellis882 <ellisrachel1304@me.com>
2021-06-14 15:10:41 +0000

    add function javascript to push the questions into availableQuestions array

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,21 @@
 
+const questionNumber = document.querySelector('.question-number');
+const questionText = document.querySelector('.question-text');
+const optionContainer = document.querySelector('.option-container');
+const answersIndicatorContainer = document.querySelector('.answers-indicator');
+const homeBox = document.querySelector('.home-box');
+const quizBox = document.querySelector('.quiz-box');
+const resultBox = document.querySelector('.result-box');
+const textBox = document.querySelector('.text-box');
+
+let questionCounter = 0;
+let currentQuestion;
+let availableQuestions = [];
+let availableOptions = [];
+let correctAnswers = 0;
+let attempt = 0;
+
+
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
@@ -10,3 +27,12 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
   }
+
+  // push the questions into availableQuestions Array
+function setAvailableQuestions(){
+    const totalQuestion = quiz.length;
+    for(let i = 0; i < totalQuestion; i++){
+        availableQuestions.push(quiz[i])
+    }    
+
+}
