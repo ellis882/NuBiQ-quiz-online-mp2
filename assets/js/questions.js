37402cb76fe14cc31665c0ddc4fb96da21e8f853
ellis882 <ellisrachel1304@me.com>
2021-06-14 14:40:51 +0000

    add quiz questions array of objects to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,65 @@
+
+// Array of Objects for Netflix quiz
+
+const quiz = [
+    {
+        q: 'What year was Netflix founded ?',
+        options: ['1990','1994','1997'],
+        answer: 2
+    },
+    {
+        q: 'Netflix began as a company that sold VHS video tapes in the post',
+        options: ['true','false'],
+        answer: 1
+    },
+    {
+        q: 'Which one of the following series WAS NOT part of the first Netflix original series programming ?',
+        options: ['House of Cards','Sons of Anarchy','Hemlock Grove','Orange is the New Black'],
+        answer: 1
+    },
+    {
+        q: '"Mini-series" is a genre of "Series" on Netflix',
+        options: ['true','false'],
+        answer: 1
+    },
+    {
+        q: 'What was Netflix originally called ?',
+        options: ['fibble','kibble','mibble'],
+        answer: 1
+    },
+    {
+        q: 'The Worlds Toughest Prisons are currently one of the most popular series on Netflix in the UK. How many seasons of the show are there ?',
+        options: ['3 seasons','4 seasons','5 seasons','7 Seasons'],
+        answer: 2
+    },
+    {
+        q: 'What is the name of the actress who plays the older Beth in the Queens Gambit ?',
+        options: ['Anya Taylor-Joy','Gemma Arteton','Isla Johnston'],
+        answer: 0
+    },
+    {
+        q: 'Which of the following reality shows is NOT currently on Netflix ?',
+        options: ['The Real Housewives of Atlanta','Keeping up with the Kardashians','Jersey Shore'],
+        answer: 2
+    },
+    {
+        q: 'What is the name of the new French Netflix serie about a professional thief who sets out the avenge his father for an injustice',
+        options: ['Lupin','Marseille','Sentinelle'],
+        answer: 0
+    },
+    {
+        q: 'in 2018, Netflix was the most-nominated studio at the Emmy Awards',
+        options: ['true','false'],
+        answer: 0
+    },
+    {
+        q: 'Which actress stars in Netflix Series, Self Made: Inspired by the Life of Madam C.J.Walker', 
+        options: ['Tiffany Haddish','Blair Underwood','Octavia Spencer'],
+        answer: 2
+    },
+    {
+        q: 'In the childrens section of Netflix, users can search for films ans shows by selecting a film character', 
+        options: ['true','false'],
+        answer: 0
+    }
+]
