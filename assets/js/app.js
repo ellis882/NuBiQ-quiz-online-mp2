2a1b61641c3731e41578a0d61b3b3282df3daffc
ellis882 <ellisrachel1304@me.com>
2021-06-14 15:29:12 +0000

    add function answersIndicator, updateAnswersIndicator and next()

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,3 +36,75 @@ function setAvailableQuestions(){
     }    
 
 }
+
+// set question number and question and options
+function getNewQuestion(){
+    // set question number
+    questionNumber.innerHTML = 'Question' + (questionCounter + 1) + 'of' + quiz.length;
+
+    // set question text
+    // get random question
+    const questionIndex = availableQuestions[Math.floor(Math.random() * availableQuestions.length)];
+    currentQuestion = questionIndex;
+    questionText.innerHTML = currentQuestion.q;
+    // get the position of 'questionIndex' from availableQuestion Array
+    const index1 = availableQuestions.indexOf(questionIndex);
+    // remove the questionIndex from the availableQuestion Array, so that the question does not repeat
+    availableQuestions.splice(index1,1);
+
+    // set options
+    // get the lenght of options
+    const optionLen = currentQuestion.options.length
+    // push options into availableOptions Array
+    for(let i = 0; i < optionLen; i++){
+        availableOptions.push(i);
+    }
+
+    optionContainer.innerHTML = '';
+    let animationDelay = 0.15;
+    // create options in html
+    for(let i = 0; i < optionLen; i++){
+        // random option
+        const optonIndex = availableOptions[Math.floor(Math.random() * availableOptions.length)];
+        // get the position of optonIndex from the availableOptions Array
+        const index2 = availableOptions.indexOf(optonIndex);
+        // remove the optonIndex from the availableOptions Array, so that the option does not repeat
+        availableOptions.splice(index2,1);
+        const option = document.createElement('div');
+        option.innerHTML = currentQuestion.options[optonIndex];
+        option.id = optonIndex;
+        option.style.animationDelay = animationDelay + 's';
+        animationDelay = animationDelay + 0.15;
+        option.className = 'option';
+        optionContainer.appendChild(option)
+        option.setAttribute('onclick', 'getResult(this)');
+    }
+
+    questionCounter++
+
+   }
+
+   function answersIndicator(){
+    answersIndicatorContainer.innerHTML = '';
+    const totalQuestion = quiz.length;
+    for( let i = 0; i < totalQuestion; i++){
+        const indicator = document.createElement('div');
+        answersIndicatorContainer.appendChild(indicator);
+    }
+
+}
+
+function updateAnswerIndicator(markType){
+    answersIndicatorContainer.children[questionCounter-1].classList.add(markType);
+
+}
+
+function next(){
+    if (questionCounter === quiz.length){
+        console.log('quiz over');
+        quizOver();
+    }
+    else{
+        getNewQuestion();
+    }
+}
